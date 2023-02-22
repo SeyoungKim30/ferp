fb4d01309fd03f0109d9c05c4c45451090990aec
leezivvon <120711596+leezivvon@users.noreply.github.com>
2023-02-22 20:56:03 +0900

    Merge branch 'jw' into main

--- src/main/java/ferp/controller/B1_Controller.java
+++ src/main/java/ferp/controller/B1_Controller.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class B1_Controller {
 
-
-	
-	// http://localhost:6080/ferp/addAccount.do
+	// http://localhost:6080/ferp/salesInfo.do
 	@RequestMapping("salesInfo.do")
 	public String r7501SalesInfo(){
 
