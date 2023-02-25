51fa0a7a7e65a56645f6397ece747b7e18380d64
som525 <som_525@naver.com>
2023-02-25 22:39:11 +0900

    menu vo 생성

---
+++ src/main/java/vo/Menu.java
@@ -0,0 +1,74 @@
+package vo;
+
+public class Menu {
+	private String menuNum;
+	private String menuName;
+	private int price;
+	private String info;
+	private String img;
+	private String necessary;
+	private String category;
+	
+	public Menu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getMenuNum() {
+		return menuNum;
+	}
+
+	public void setMenuNum(String menuNum) {
+		this.menuNum = menuNum;
+	}
+
+	public String getMenuName() {
+		return menuName;
+	}
+
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getNecessary() {
+		return necessary;
+	}
+
+	public void setNecessary(String necessary) {
+		this.necessary = necessary;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+}
