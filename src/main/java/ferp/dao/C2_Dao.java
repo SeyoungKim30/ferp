a509611b4600790ff99e42efa9a16a371945afc7
arongss <113689035+arongss@users.noreply.github.com>
2023-02-27 04:08:34 +0900

    ar

--- src/main/java/ferp/dao/C2_Dao.java
+++ src/main/java/ferp/dao/C2_Dao.java
@@ -3,10 +3,12 @@ package ferp.dao;
 import java.util.List;
 
 import vo.Product;
+import vo.ProductProdOrder;
 
 public interface C2_Dao {
-	public List<Product> r8101ProductSch(Product sch);
-	public List<Product> r8201ProductList(Product sch);
+	public List<ProductProdOrder> r8101ProductList(ProductProdOrder sch);
+	public List<ProductProdOrder> r8201ProductList(ProductProdOrder sch);
+	public List<String> orderStateCom();
 	public Product r8201ProductInfo(String productNum);	
 	public void r8202ProductIns(Product ins);
 }
