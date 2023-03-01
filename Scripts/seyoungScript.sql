4d1326070b8af4c88e0b7c220d762b62cdab5483
sengu30 <dearseng@gmail.com>
2023-03-01 15:49:18 +0900

    발주검색

--- Scripts/seyoungScript.sql
+++ Scripts/seyoungScript.sql
@@ -81,7 +81,7 @@ AND STMTDATE BETWEEN TO_DATE('2023-02-01','YYYY-MM-DD') AND TO_DATE('2023-02-10'
 AND FRREGINUM = 'fakeID1111'
 	GROUP BY STMTDATE;
 
-SELECT TRUNC(orderdate) FROM PRODORDER p ;
+SELECT * FROM PRODORDER p ;
 /*CREATE SEQUENCE pdSeq
 INCREMENT BY 1
 START WITH 10000
@@ -120,11 +120,28 @@ AND ORDERSTATE LIKE '%'||''||'%'
 AND TRUNC(orderdate) = to_date('2023-02-25','YYYY-MM-DD')
 ;
 
---실시간 본사 재고
-SELECT * FROM STOCK s WHERE STOCKDATE IN (SELECT max(STOCKDATE) FROM stock GROUP BY PRODUCTNUM  );
+--발주 검색: 최종
+SELECT po.*,se.ename,se.frname,stck.remainamount FROM PRODORDER po, product pd, 
+		(SELECT ename,FRREGINUM ,e.empnum,frname,FRREPNAME  FROM store s, emp e WHERE s.EMPNUM =e.EMPNUM) se,
+		(SELECT * FROM STOCK s WHERE STOCKDATE IN (SELECT max(STOCKDATE) FROM stock GROUP BY PRODUCTNUM)) stck
+WHERE po.DEMANDER = se.FRREGINUM AND pd.PRODUCTNUM =po.PRODUCTNUM AND stck.productNum = po.PRODUCTNUM 
+	AND (TRUNC(orderdate) = to_date('','YYYY-MM-DD')	--일별일때
+			OR TRUNC(orderdate,'month') = TRUNC(to_date('','YYYY-MM-DD'),'month')--월별일때
+			OR ORDERNUM LIKE '%'||'2023021234567890'||'%')	--발주번호
+	AND (po.DEMANDER LIKE '%'||'디맨더로 받아온 변수'||'%' OR se.frname LIKE '%'||'독산'||'%')	--주문지점
+	AND (se.ename LIKE '%'||''||'%' OR se.empNum LIKE '%'||''||'%')	--담당자
+	AND (pd.PRODUCTNUM LIKE '%'||''||'%' OR pd.PRODUCTNAME LIKE '%'||''||'%') --상품번호 또는 이름
+	AND po.PAYMENTSTATE LIKE '%'||''||'%' AND po.ORDERSTATE LIKE '%'||''||'%'
+;
+
+--서브테이블
+SELECT se.* FROM (SELECT FRREGINUM ,e.empnum,frname,FRREPNAME FROM store s, emp e WHERE s.EMPNUM =e.EMPNUM) se;
+--서브테이블 실시간 본사 재고
+SELECT * FROM STOCK s WHERE STOCKDATE IN (SELECT max(STOCKDATE) FROM stock GROUP BY PRODUCTNUM);
 
 SELECT * FROM stock;
-SELECT * FROM emp;
+SELECT * FROM emp  ;
 /*INSERT INTO emp values('00009999','1234','admin','관리자');
 INSERT INTO store(frreginum) values('9999999999',); 
 INSERT INTO stock VALUES ('PD10001','9999999999',sysdate,100,100,null);*/
+INSERT INTO stock VALUES ('PD10001','9999999999',sysdate,10,110,null);
