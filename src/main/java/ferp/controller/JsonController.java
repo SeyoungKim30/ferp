9b747d6e4a199802450b58312c09990bbc2dd092
sengu30 <dearseng@gmail.com>
2023-03-04 22:27:20 +0900

    9201수정

--- src/main/java/ferp/controller/JsonController.java
+++ src/main/java/ferp/controller/JsonController.java
@@ -7,19 +7,30 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import ferp.service.C1_Service;
-import vo.Account;
+import ferp.service.C2_Service;
+import vo.ProductProdOrder;
 
 @Controller
 public class JsonController {
 
 	@Autowired
-	C1_Service service;
+	C1_Service servicec1;
+	@Autowired
+	C2_Service servicec2;
 	
 	@CrossOrigin(origins = "*",allowedHeaders = "*")
 	@GetMapping("selectEmpListJson.do")
 	public String selectEmpListJson(Model model) {
-		//model.addAttribute();
+		//model.addAttribute(); 추가예정
 		return "pageJsonReport";
 	}
+	
+	@CrossOrigin(origins = "*",allowedHeaders = "*")
+	@GetMapping("selectProdListJson.do")
+	public String selectProdListJson(Model model) {
+		//model.addAttribute("productList",servicec2.r8101ProductInfo("")); 번호검색 like아니라서 보류
+		return "pageJsonReport";
+	}
+	
 	
 }
