a6ada80c83d5f793351c508c2b6cedf9a671f1d8
leezivvon <120711596+leezivvon@users.noreply.github.com>
2023-03-08 02:34:22 +0900

    새벽 중간점검.

--- src/main/java/ferp/dao/B1_Dao.java
+++ src/main/java/ferp/dao/B1_Dao.java
@@ -2,18 +2,23 @@ package ferp.dao;
 
 import java.util.List;
 
+import vo.OpenTimeCalender;
 import vo.Orders;
 import vo.Store;
 
 public interface B1_Dao {
 	
+	//매장정보조회
 	public int lastmonthAllSales(); //본사:지난달전체매장매출총액
-	public List<Orders> SalesByStoreList(Orders sbsl);	//본사:전매장매출 전체조회
+	public List<Orders> salesByStoreList(Orders sbsl);	//본사:전매장매출-전체조회
 	
-	public Orders StoreDetailInfo(String frRegiNum); //본사:매장정보상세조회
-	public List<Orders> DetailSalesList(Orders dinfo); //본사:매장정보상세조회-매매액
-	public List<Orders> DetailMenuList(Orders dinfo); //본사:매장정보상세조회-메뉴
+	public Orders storeDetailInfo(String frRegiNum); //본사:매장정보상세조회
+	public List<Orders> detailSalesList(Orders dinfo); //본사:매장정보상세조회-매매액
+	public List<Orders> detailMenuList(Orders dinfo); //본사:매장정보상세조회-메뉴
 	
-	public List<Store> StoreOpenList(Store otl); //본사:전매장오픈시간조회
-	public Store StoreDetailOpenTime(String frRegiNum); //본사:매장오픈상세조회
+	//매장오픈점검
+	public List<Store> storeOpenList(Store otl); //본사:전매장오픈시간-전체조회
+	
+	public Store storeDetailOpenTime(String frRegiNum); //본사:매장오픈상세조회
+	public List<OpenTimeCalender> openTimeDetailCalendar(String writer); //본사:매장오픈상세조회-캘린더
 }
--- src/main/java/vo/OpenTimeCalender.java
+++ src/main/java/vo/OpenTimeCalender.java
@@ -1,53 +1,31 @@
 package vo;
 
 public class OpenTimeCalender {
-	/*
-	private String frRegiNum;
-	private String frOperTime;
-	private String frClosedDate;
-	private String frRepName;
-	private String ename;
 	
 	private String writer;
 	private String start; //onTime출근시간
+	private String end; //offTime출근시간
 	
-	public String getFrName() {
-		return frName;
-	}
-	public void setFrName(String frName) {
-		this.frName = frName;
-	}
-	public String getFrOperTime() {
-		return frOperTime;
-	}
-	public void setFrOperTime(String frOperTime) {
-		this.frOperTime = frOperTime;
-	}
-	public String getFrClosedDate() {
-		return frClosedDate;
-	}
-	public void setFrClosedDate(String frClosedDate) {
-		this.frClosedDate = frClosedDate;
-	}
-	public String getFrRepName() {
-		return frRepName;
-	}
-	public void setFrRepName(String frRepName) {
-		this.frRepName = frRepName;
-	}
-	public String getEname() {
-		return ename;
-	}
-	public void setEname(String ename) {
-		this.ename = ename;
-	}
+	
 	public String getStart() {
 		return start;
 	}
 	public void setStart(String start) {
 		this.start = start;
 	}
-	*/
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+	
 	
 
 }
