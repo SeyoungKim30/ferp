618d3a80a8f577e79c49a6d5ffdd28c9bd3b0e54
JungHwyKim <kkjjhh31@hanmail.net>
2023-03-13 18:04:14 +0900

    vo

---
+++ src/main/java/vo/Sales.java
@@ -0,0 +1,18 @@
+package vo;
+
+public class Sales {
+	private int tot;
+	private String orderdate;
+	public int getTot() {
+		return tot;
+	}
+	public void setTot(int tot) {
+		this.tot = tot;
+	}
+	public String getOrderdate() {
+		return orderdate;
+	}
+	public void setOrderdate(String orderdate) {
+		this.orderdate = orderdate;
+	}
+}
